36b521c4307b777454a8034dbdd4c4a173de224e
JawadKhoja <95450953+JawadKhoja@users.noreply.github.com>
2024-11-23 03:33:58 -0500

    Update server.java

--- src/server.java
+++ src/server.java
@@ -6,8 +6,8 @@ import java.rmi.registry.LocateRegistry;
 public class server {
     public static void main(String[] args) {
         try {
-            // Start the RMI registry on port 1099
-            LocateRegistry.createRegistry(1099);
+            // Start the RMI registry on port 1098
+            LocateRegistry.createRegistry(1098);
 
             // Create an instance of the MovieInformation service
             MovieInformation movieService = new MovieInformation();
